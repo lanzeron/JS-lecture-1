2c7484dc5465604a72875fbf05f8ad910e1dd9ac
Yura Kovalchuk <lanzeron@gmail.com>
2015-12-19 14:01:14 +0200

    update my homework

---
+++ js/for.js
@@ -0,0 +1,8 @@
+"use striet";
+(function() {
+    var output = '';
+    for (var i = 0; i < 10; i++) {
+        output = output + '#';
+        console.log(output)
+    };
+})();
---
+++ js/legnth.js
@@ -0,0 +1,9 @@
+"use striet";
+(function() {
+    var x2 = prompt("please enter coordinate X", "0");
+    var y2 = prompt("please enter coordinate Y", "0");
+    var x1 = 5;
+    var y1 = 6;
+    var result = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) + (y1 - y2));
+    alert(result);
+})();
